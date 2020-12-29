b6fb09672e2fd8a1eaa740829e01cb6afdee64c6
WeihaiYu <1600219867@qqcom>
2020-12-29 14:03:55 +0800

    build

---
+++ schedule-frontend/build/precache-manifest.dae0fa11d8205ad0a84a93728e4ffc5e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11ae00d4ca9ff906f3fb58075d044b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09401eb6ece211ca45e",
+    "url": "/static/css/2.e6e2d61c.chunk.css"
+  },
+  {
+    "revision": "0dea2b95f908b23af372",
+    "url": "/static/css/main.b6f1804a.chunk.css"
+  },
+  {
+    "revision": "e09401eb6ece211ca45e",
+    "url": "/static/js/2.74f8ebba.chunk.js"
+  },
+  {
+    "revision": "ff6f1f3be7ff3c092017e2974c44a438",
+    "url": "/static/js/2.74f8ebba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dea2b95f908b23af372",
+    "url": "/static/js/main.980a0d34.chunk.js"
+  },
+  {
+    "revision": "24f67345e9359da1b0c4",
+    "url": "/static/js/runtime-main.2c9e89a7.js"
+  },
+  {
+    "revision": "c2a7a22f738ecf43bd39f341ca584e81",
+    "url": "/static/media/img_kaikeba_logo_blue_new.c2a7a22f.png"
+  },
+  {
+    "revision": "4cb192622c14a4d44dd8282300593cc5",
+    "url": "/static/media/放假时间管理.4cb19262.png"
+  },
+  {
+    "revision": "1c0e82c7f7a605ef3a21e76f7c948ef2",
+    "url": "/static/media/新建假期.1c0e82c7.png"
+  },
+  {
+    "revision": "cc32b4b201df051241875a778d2dd018",
+    "url": "/static/media/添加单节课程.cc32b4b2.png"
+  },
+  {
+    "revision": "a690d525c847e2a4e71f3eba0f250ef2",
+    "url": "/static/media/添加课程-设置基本.a690d525.png"
+  },
+  {
+    "revision": "fde142d9748c455e4de6d843618e5875",
+    "url": "/static/media/添加课程-设置讲师.fde142d9.png"
+  },
+  {
+    "revision": "cf3b303cc64302f9ec6e0f712b24bd51",
+    "url": "/static/media/添加课程-调整课程.cf3b303c.png"
+  },
+  {
+    "revision": "e84402d28d624332512032d2be39c967",
+    "url": "/static/media/讲师管理.e84402d2.png"
+  },
+  {
+    "revision": "5f43dfeb07d075e03a18b8546893af06",
+    "url": "/static/media/课程管理.5f43dfeb.png"
+  },
+  {
+    "revision": "7230cd5572dd190298382b2569f40127",
+    "url": "/static/media/首页.7230cd55.png"
+  }
+]);
